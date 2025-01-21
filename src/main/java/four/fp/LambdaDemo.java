c6c3f524aa09a2f3abbe47eef7589049f2b7bee8
Emir Puska <emir.puska@it-akademija.com>
2025-01-21 20:46:29 +0100

    Lambda code explanation

--- src/main/java/four/fp/LambdaDemo.java
+++ src/main/java/four/fp/LambdaDemo.java
@@ -4,6 +4,9 @@ import three.customer.Customer;
 import three.customer.CustomerDao;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class LambdaDemo {
     public static void main(String[] args) {
@@ -16,7 +19,8 @@ public class LambdaDemo {
         // metode -> pozovemo ih nad stream objektom i one opet vrate stream objekat / u većini slučajeva izmijenjen
         //3. terminated operation -> terminirajuće metode -> pozovemo ih nad stream objektom a one više ne vrate stream objekat
         // pa za njih zato kažemo da terminiraju stream
-        // Martin Fowler
+
+        // Martin Fowler STYLE
         customers
                 .stream()
                 .filter(c -> c.getPhone() != null)
@@ -25,5 +29,15 @@ public class LambdaDemo {
                 .map(customer -> customer.setPoints(customer.getPoints() + 1000))
                 .forEach(customer -> System.out.println(customer));
 
+
+        //Razložiti-bez Martin Fowler stila
+        Stream<Customer> customerStream = customers.stream();
+        Predicate<Customer> phoneTester = c -> c.getPhone() != null;
+        customerStream = customerStream.filter(phoneTester);
+        Predicate<Customer> customerWithPointTester = c -> c.getPoints() > 0;
+        customerStream = customerStream.filter(customerWithPointTester);
+        Function<Customer, Customer> customerTransformer = c -> c.setPoints(c.getPoints() + 1000);
+        customerStream = customerStream.map(customerTransformer);
+        customerStream.forEach(customer -> System.out.println(customer));
     }
 }
