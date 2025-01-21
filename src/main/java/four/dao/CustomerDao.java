81d16e74647b6b9c0e74feb128e5b317e95801d4
Emir Puska <emir.puska@it-akademija.com>
2025-01-21 19:45:02 +0100

    Complete set of CRUD operations

--- src/main/java/four/dao/CustomerDao.java
+++ src/main/java/four/dao/CustomerDao.java
@@ -5,6 +5,8 @@ import two.db.dao.ConnectionPool;
 import two.db.dao.Dao;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -52,12 +54,40 @@ public class CustomerDao implements Dao<Customer> {
         } finally {
             ConnectionPool.instance().releaseConnection(connection);
         }
-
-        return null;
+        return customer;
     }
 
     @Override
     public List<Customer> findAll() {
+        //1. Connection
+        Connection connection = ConnectionPool.instance().getConnection();
+        //2. Statement
+        try (PreparedStatement statement = connection.prepareStatement("select * from customers")) {
+            //3. ResultSet
+            ResultSet resultSet = statement.executeQuery();
+            List<Customer> customers = new ArrayList<>();
+            while (resultSet.next()) {
+                Customer customer = new Customer();
+                customer.setCustomerId(resultSet.getInt("customer_id"));
+                customer.setFirstName(resultSet.getString("first_name"));
+                customer.setLastName(resultSet.getString("last_name"));
+                Date birthDate = resultSet.getDate("birth_date");
+                LocalDate localBirthDate = birthDate.toLocalDate();
+                customer.setBirthDate(localBirthDate);
+                customer.setPhone(resultSet.getString("phone"));
+                customer.setAddress(resultSet.getString("address"));
+                customer.setCity(resultSet.getString("city"));
+                customer.setState(resultSet.getString("state"));
+                customer.setPoints(resultSet.getInt("points"));
+                customers.add(customer);
+            }
+            return customers;
+
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            ConnectionPool.instance().releaseConnection(connection);
+        }
         return List.of();
     }
 
